f3abdaa1ef19ad6bf03d20fa5ca888ee5e6eb2bb
Jennifer Duong <jen@cautionyourblast.com>
2017-12-21 13:37:18 +0000

    template builder/renderer

--- src/server/server.js
+++ src/server/server.js
@@ -1,70 +1,38 @@
 import 'svelte/ssr/register';
 import 'svelte';
-import 'fs';
+import fs from 'fs';
 import 'path';
-import appStart from './../../components/AppStart.html';
 import Search from './../../components/Search.html';
-
+import Template from './../client/index.html';
 
 let template = fs.readFileSync( `${__dirname}/../client/index.html`, 'utf-8' );
-const templateChunks = [];
-const pattern = /__(\w+)__/g;
-let match;
-let c = 0;
 
-while ( match = pattern.exec( template ) ) {
-    templateChunks.push({
-        type: 'static',
-        content: template.slice( c, match.index )
+
+
+function writePageWith(renderedComponents){
+    var appParts;
+    renderedComponents.forEach(function(component){
+        appParts += component.toString()
     });
 
-    templateChunks.push({
-        type: 'dynamic',
-        content: match[1]
+    const temphtml = Template.render({
+        app: appParts
     });
 
-    c = match.index + match[0].length;
+    return temphtml
+
+
 }
-
-templateChunks.push({
-    type: 'static',
-    content: template.slice( c )
-});
-function serve ( res, data) {
-    res.writeHead( 200, {
-        'Content-Type': 'text/html'
-    });
-
-    let promise = Promise.resolve();
-    templateChunks.forEach( chunk => {
-        promise = promise.then( () => {
-            if ( chunk.type === 'static' ) {
-                res.write( chunk.content );
-            }
-
-            else {
-                return Promise.resolve( data[ chunk.content ] ).then( content => {
-                    res.write( content );
-                });
-            }
-        });
-    });
-
-    return promise.then( () => {
-        res.end();
-    });
-}
-
 
 
 
 
 export default function middleware(req, res) {
-    serve( res, {
-        route: About.render()
-    } );
-    //res.send(Search.render().html)
-//res.send(appStart.render({name: "world"}).html);
+
+
+    const app = writePageWith([Search.render().html, Search.render().html]);
+    res.send(app.html);
+
 }
 
 
