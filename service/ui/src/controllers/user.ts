05fdfa4441c371c1ec4ad69d46483d0dd03f6134
Jennifer Duong <jen@cautionyourblast.com>
2018-01-22 16:53:09 +0000

    install request, change user/profile form, add server route + call to wso2 (WIP)

--- service/ui/src/controllers/user.ts
+++ service/ui/src/controllers/user.ts
@@ -1,6 +1,7 @@
 import * as config from 'config'
 import {Request, Response} from 'express'
 import * as template from 'ui/template'
+import * as request from 'request'
 
 export let signIn = (req: Request, res: Response) => {
 	const sessionDataKey = req.query.sessionDataKey
@@ -53,5 +54,36 @@ function renderSignIn(props: SignIn) {
 }
 
 function renderProfile(props: User) {
+	console.log(props)
 	return template.render('profile', props)
+}
+
+export let updateUser = (req: Request, res: Response) => {
+	let headers = {
+		'Content-Type': 'application/json',
+	}
+	let options = {
+		uri:
+			config.get('authentication.serviceUrl') + '/scim2/Users/' + req.user.id,
+		method: 'PUT',
+		headers: headers,
+		body: JSON.stringify(req.body),
+		auth: {
+			user: config.get('authentication.serviceAdmin'),
+			pass: config.get('authentication.servicePassword'),
+		},
+	}
+	console.log(JSON.stringify(req.body))
+	console.log(options)
+	// res.send()
+
+	request(options, function(error: Error, response: Response, body: JSON) {
+		if (!error && response.statusCode == 200) {
+			console.log('done')
+			console.log(body) // Print the shortened url.
+			res.send(body)
+		} else {
+			res.send(console.log(error))
+		}
+	})
 }
