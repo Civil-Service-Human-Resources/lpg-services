60f390b30c623673b7976c2a9b6f8d7445909813
Max Stewart <max@cautionyourblast.com>
2018-01-24 12:28:02 +0000

    Edit profile success page.

--- service/ui/src/controllers/search.ts
+++ service/ui/src/controllers/search.ts
@@ -4,11 +4,11 @@ import * as template from 'ui/template'
 
 export let index = (req: Request, res: Response) => {
 	let profile: Profile = {user: req.user}
-    if (!req.user.profession) {
-        res.redirect('/profile')
-    } else {
-        res.send(renderSearch(profile))
-    }
+	if (!req.user.profession) {
+		res.redirect('/profile')
+	} else {
+		res.send(renderSearch(profile))
+	}
 }
 
 function renderSearch(props: Profile) {
