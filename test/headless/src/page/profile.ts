5e5731de53a510204ea25b0ef4eb87109b1d7ae3
William Pritchard <williamp@kainos.com>
2018-01-24 10:36:05 +0000

    Profile fields checker

--- test/headless/src/page/profile.ts
+++ test/headless/src/page/profile.ts
@@ -2,4 +2,7 @@
 
 export const selectors: {[key: string]: string} = {
 	feedbackLink: '#content > div.phase-banner > p > span > a',
+	department: '#department',
+	profession: '#profession',
+	grade: '#grade',
 }
