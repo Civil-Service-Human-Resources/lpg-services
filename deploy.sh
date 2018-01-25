3ca9d78364988622f0f1bfc4c2066671c9b3666b
silvanodossan <silvano@superroot.eu>
2018-01-25 11:47:03 +0000

    testing deploy

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,16 @@
 repo=${1}
 key=${2}
 
+echo "set ${key} version to $TRAVIS_COMMIT"
+echo "lpgui: $TRAVIS_COMMIT"
+
 cat >> ~/.ssh/config << EOF
 Host	github.com
 	Hostname        github.com
-	IdentityFile    "github"
+	IdentityFile    "$TRAVIS_BUILD_DIR/github"
 	IdentitiesOnly	yes
 EOF
-
+cat ~/.ssh/config
 chmod 600 github
 git clone git@github.com:Civil-Service-Human-Resources/${repo}.git
 cd lpg-ansible-mvp-services
