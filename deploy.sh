8dc320532b5fd3eb2064de713dc3cf029f92a253
silvanodossan <silvano@superroot.eu>
2018-01-25 11:43:14 +0000

    testing deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 repo=${1}
 key=${2}
 
-cat > ~/.ssh/config << EOF
+cat >> ~/.ssh/config << EOF
 Host	github.com
 	Hostname        github.com
 	IdentityFile    "github"
@@ -11,11 +11,8 @@ Host	github.com
 EOF
 
 chmod 600 github
-ls -la
 git clone git@github.com:Civil-Service-Human-Resources/${repo}.git
-ls -la
 cd lpg-ansible-mvp-services
-ls -la
 git checkout test
 echo "lpgui: $TRAVIS_COMMIT" > group_vars/all/services
 git commit -am "set ${key} version to $TRAVIS_COMMIT"
