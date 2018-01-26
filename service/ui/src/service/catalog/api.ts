5b0336540cea7dbab2113392080716935420a51a
tav <tav@espians.com>
2018-01-26 14:02:45 +0000

    service/ui: mimic the autogenerated api type definitions

---
+++ service/ui/src/service/catalog/api.ts
@@ -0,0 +1,16 @@
+export interface Entry {
+	tags?: string[]
+	title?: string
+	uid?: string
+	uri?: string
+}
+
+export interface SearchRequest {
+	tags?: string[]
+	first?: number
+	after?: string
+}
+
+export interface SearchResponse {
+	entries: Entry[]
+}
