07b4538e330d09287c545878ad2cae7aba6d3eba
William Pritchard <williamp@kainos.com>
2018-02-01 17:38:38 +0000

    Added queryUser to fetch userId

--- test/headless/src/page/profile.ts
+++ test/headless/src/page/profile.ts
@@ -2,6 +2,7 @@ import * as puppeteer from 'puppeteer'
 
 export const selectors: Record<string, string> = {
 	feedbackLink: '#content > div.phase-banner > p > span > a',
+	userName: '#userName',
 	department: '#department',
 	profession: '#profession',
 	grade: '#grade',
@@ -9,15 +10,29 @@ export const selectors: Record<string, string> = {
 	saveProfileButton: '#content > div.main-content > div > div > form > input',
 	signoutButton: '#proposition-links > li:nth-child(3) > a',
 	profilePageButton: '#proposition-links > li:nth-child(2) > a',
-	incompleteProfileError: '#error-summary-heading-example-1',
+	incompleteProfileError: '#content > div.main-content > div > div > div',
 	updateProfileError:
 		'#content > div.main-content > div > div > div > p:nth-child(2)',
+	departmentFieldError:
+		'#content > div.main-content > div > div > form > div:nth-child(2) > span',
+	professionFieldError:
+		'#content > div.main-content > div > div > form > div:nth-child(3) > span',
+	gradeFieldError:
+		'#content > div.main-content > div > div > form > div:nth-child(4) > span',
 }
 
 export async function setUserProfileDetails(page: puppeteer.Page) {
 	await page.type(selectors.department, 'department')
 	await page.type(selectors.profession, 'profession')
 	await page.type(selectors.grade, 'grade')
+	await page.click(selectors.saveProfileButton)
+}
+
+export async function setProfileFieldToEmptyAndSave(
+	selector: string,
+	page: puppeteer.Page
+) {
+	await page.type(selector, ' ')
 	await page.click(selectors.saveProfileButton)
 }
 
