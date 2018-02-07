0f70210cad47a898a2402cf1a63d5d335f2a536c
Max Stewart <max@cautionyourblast.com>
2018-02-07 09:28:59 +0000

    Learning record page and course complete page.

--- service/ui/src/controllers/learning-record/index.ts
+++ service/ui/src/controllers/learning-record/index.ts
@@ -1,14 +1,17 @@
+import * as axios from 'axios'
 import {Request, Response} from 'express'
 import * as log4js from 'log4js'
 import * as config from 'lib/config'
+import * as catalog from 'lib/service/catalog'
 import * as template from 'lib/ui/template'
-import * as axios from 'axios'
 
 const logger = log4js.getLogger('controllers/learning-record/index')
 
 export async function display(req: Request, res: Response) {
 	logger.debug(`Displaying learning record for ${req.user.id}`)
-	res.send(template.render('learning-record', req))
+	res.send(template.render('learning-record', req, {
+		courses: await getLearningRecord(req.user)
+	}))
 }
 
 export async function courseResult(req: Request, res: Response) {
@@ -18,28 +21,8 @@ export async function courseResult(req: Request, res: Response) {
 		}`
 	)
 
-	const agent = {
-		mbox: `mailto:${req.user.emailAddress}`,
-		name: req.user.id,
-		objectType: 'Agent',
-	}
-
 	try {
-		const response = await axios({
-			auth: config.XAPI.auth,
-			headers: {
-				'X-Experience-API-Version': '1.0.1',
-			},
-			method: 'get',
-			params: {
-				activity: req.course.uri,
-				agent: JSON.stringify(agent),
-			},
-			url: `${config.XAPI.url}/statements`,
-		})
-
-		const statements = response.data.statements
-		const state = getState(statements)
+		const {state, result} = await getCourseRecord(req.user, req.course)
 
 		if (!state) {
 			res.redirect('/courses/' + req.params.courseId)
@@ -47,7 +30,7 @@ export async function courseResult(req: Request, res: Response) {
 			res.send(
 				template.render('learning-record/course-result', req, {
 					course: req.course,
-					result: getResult(statements),
+					result,
 					state,
 				})
 			)
@@ -55,6 +38,80 @@ export async function courseResult(req: Request, res: Response) {
 	} catch (e) {
 		logger.error('Error retrieving learner record', e)
 		res.sendStatus(500)
+	}
+}
+
+async function getLearningRecord(user: any) {
+	const agent = {
+		mbox: `mailto:${user.emailAddress}`,
+		name: user.id,
+		objectType: 'Agent',
+	}
+
+	const response = await axios({
+		auth: config.XAPI.auth,
+		headers: {
+			'X-Experience-API-Version': '1.0.1',
+		},
+		method: 'get',
+		params: {
+			agent: JSON.stringify(agent),
+		},
+		url: `${config.XAPI.url}/statements`,
+	})
+
+	const statements = response.data.statements
+	const courses = []
+
+	const groupedStatements = {}
+
+	for (const statement of statements) {
+		const key = statement.object.id
+		if (!groupedStatements[key]) {
+			groupedStatements[key] = []
+		}
+		groupedStatements[key].push(statement)
+	}
+
+	for (const key in groupedStatements) {
+		const statements = groupedStatements[key]
+		const state = getState(statements)
+		if (state === 'completed') {
+			const result = getResult(statements)
+			const course = await catalog.findCourseByUri(key)
+			courses.push({...course, result, state})
+		}
+	}
+	return courses
+}
+
+async function getCourseRecord(user: any, course: any) {
+	const agent = {
+		mbox: `mailto:${user.emailAddress}`,
+		name: user.id,
+		objectType: 'Agent',
+	}
+
+	const response = await axios({
+		auth: config.XAPI.auth,
+		headers: {
+			'X-Experience-API-Version': '1.0.1',
+		},
+		method: 'get',
+		params: {
+			activity: course.uri,
+			agent: JSON.stringify(agent),
+		},
+		url: `${config.XAPI.url}/statements`,
+	})
+
+	const statements = response.data.statements
+	const state = getState(statements)
+	const result = getResult(statements)
+
+	return {
+		result,
+		state,
 	}
 }
 
