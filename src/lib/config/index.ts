d6fd5ce799d6e4c2ba81369186e60f7df6117905
Max Stewart <max@cautionyourblast.com>
2018-02-12 15:49:01 +0000

    WSO2 is https again...

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -20,7 +20,7 @@ export const AUTHENTICATION = set({
 	servicePassword: 'admin',
 	serviceUrl:
 		process.env.AUTHENTICATION_SERVICE_URL ||
-		'http://identity.dev.cshr.digital:9443',
+		'https://identity.dev.cshr.digital:9443',
 })
 
 export const LOGGING = set(
