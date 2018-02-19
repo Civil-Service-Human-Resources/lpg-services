c407f6d967bffad15560efc78cb0102f573f4f94
tav <tav@espians.com>
2018-02-19 13:53:33 +0000

    src/ui: implement page feedback handling

--- src/ui/assets/js/main.js
+++ src/ui/assets/js/main.js
@@ -5,4 +5,27 @@ $(document).ready(function() {
 		$(this).toggleClass('view-all--showing')
 		$('#view-all').toggleClass('js-hidden')
 	})
+	// Install click handler to reveal the feedback form.
+	var feedbackRevealed = false
+	$('.feedback-prompt').click(function(e) {
+		e.preventDefault()
+		if (feedbackRevealed) {
+			return
+		}
+		feedbackRevealed = true
+		var details = $('.feedback-details')[0]
+		details.style.display = 'block'
+		$('#whatDoing').focus()
+		if (details.scrollIntoView) {
+			details.scrollIntoView()
+		}
+	})
+	// Install submit handler to intercept the feedback submission.
+	$('.feedback form').submit(function(e) {
+		e.preventDefault()
+		// TODO(tav): Handle error state
+		$.post('/feedback.record', $('.feedback form').serialize(), function(resp) {
+			$('.feedback-details')[0].innerHTML = resp
+		})
+	})
 })
---
+++ src/ui/controllers/feedback.ts
@@ -0,0 +1,34 @@
+import * as express from 'express'
+import * as feedback from 'lib/feedback'
+import * as log4js from 'log4js'
+
+const logger = log4js.getLogger('controllers/feedback')
+
+export async function record(req: express.Request, res: express.Response) {
+	const {pageUrl = '', wentWrong = '', whatDoing = ''} = req.body
+	if (!pageUrl || !pageUrl.startsWith('/')) {
+		res.sendStatus(500)
+		return
+	}
+	if (wentWrong || whatDoing) {
+		const param = {pageUrl, wentWrong, whatDoing}
+		try {
+			// const a = 2
+			await feedback.record(param)
+		} catch (err) {
+			logger.error(
+				`Couldn't record feedback: ${JSON.stringify(param)}
+
+Got error: ${err}`
+			)
+		}
+	}
+	if (req.xhr) {
+		res.send(`<h2>Thank you for your help.</h2>
+<p>If you have more extensive feedback, please <a href="mailto:feedback@cslearning.gov.uk">send us an email</a>.</p>
+    `)
+		return
+	}
+	req.session!.flash = 'Thank you for your feedback!'
+	res.redirect(pageUrl)
+}
