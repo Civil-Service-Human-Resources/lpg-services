228126aaed1dff5ea2289253c7111e8e6c18b6ad
William Pritchard <williamp@kainos.com>
2018-02-22 11:10:44 +0000

    added new env lookup and delete user

--- test/headless/src/test/recurringCourses.ts
+++ test/headless/src/test/recurringCourses.ts
@@ -1,19 +1,11 @@
 import * as puppeteer from 'puppeteer'
 import * as helper from 'extension/helper'
 import {wrappedBeforeAll, wrappedAfterAll} from 'extension/testsetup'
-import {createUser, updateUser} from 'extension/user'
+import {createUser, deleteUser, getUser, updateUser} from 'extension/user'
 import * as xapi from 'extension/xapi'
 import {loginToCsl} from 'page/login'
 import {selectors} from 'page/learningPlan'
-
-const {
-	URL = '',
-	USERNAME = '',
-	PASS = '',
-	TEST_PASSWORD = '',
-	DIALOG_USERNAME = '',
-	DIALOG_PASSWORD = '',
-} = process.env
+import * as config from 'test/config'
 
 function genUserEmail() {
 	return `test${Date.now()}@c.gov.uk`
@@ -29,17 +21,19 @@ describe('recurring courses functionality', () => {
 		const session = await helper.getSession('recurring courses')
 		page = await session.newPage()
 		await page.authenticate({
-			username: DIALOG_USERNAME,
-			password: DIALOG_PASSWORD,
+			username: config.BASIC_AUTH_USERNAME,
+			password: config.BASIC_AUTH_PASSWORD,
 		})
-		await page.goto(URL)
-		userId = await createUser(TEST_USERNAME, TEST_PASSWORD)
+		await page.goto(config.URL)
+		userId = await createUser(TEST_USERNAME, config.TEST_PASSWORD)
 		await updateUser(userId, TEST_USERNAME, 'Test', 'co', 'commercial', 'G7')
-		await loginToCsl(page, USERNAME, PASS)
+		await loginToCsl(page, TEST_USERNAME, config.TEST_PASSWORD)
 		await page.waitFor(selectors.signoutButton)
 	})
 
 	wrappedAfterAll(async () => {
+		const userInfo = await getUser(TEST_USERNAME)
+		await deleteUser(userInfo.id)
 		await page.close()
 	})
 
@@ -49,29 +43,29 @@ describe('recurring courses functionality', () => {
 	})
 
 	it('Should hide completed course before required by date', async () => {
-
-        expect(await helper.checkElementIsPresent(selectors.counterFraudCourse, page)).toBe(true)
+		expect(
+			await helper.checkElementIsPresent(selectors.counterFraudCourse, page)
+		).toBe(true)
 
 		// Counter fraud, ...
-		await xapi.addStatement(
-			userId,
-			'0x390',
-			xapi.Verb.Completed,
-			new Date()
-		)
+		await xapi.addStatement(userId, '0x390', xapi.Verb.Completed, new Date())
 
-        expect(await helper.checkElementIsPresent(selectors.counterFraudCourse, page)).toBe(false)
-    })
+		expect(
+			await helper.checkElementIsPresent(selectors.counterFraudCourse, page)
+		).toBe(false)
+	})
 
 	it('Should show completed course after previous required by date passes', async () => {
 		// Basic fire awareness
 		await xapi.addStatement(
-            userId,
+			userId,
 			'0x394',
 			xapi.Verb.Completed,
 			new Date(2017, 0, 1)
 		)
 
-		expect(await helper.checkElementIsPresent(selectors.fireAwarenessCourse, page)).toBe(true)
+		expect(
+			await helper.checkElementIsPresent(selectors.fireAwarenessCourse, page)
+		).toBe(true)
 	})
 })
