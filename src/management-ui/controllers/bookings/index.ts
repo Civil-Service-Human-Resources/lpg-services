0eae1c0db5471d0e09e64b528a8ff999facd3f2c
Jennifer Duong <jen@cautionyourblast.com>
2018-02-27 12:22:13 +0000

    list registrations

--- src/management-ui/controllers/bookings/index.ts
+++ src/management-ui/controllers/bookings/index.ts
@@ -1,10 +1,10 @@
 import {Request, Response} from 'express'
-import * as catalog from 'lib/service/catalog'
-import * as log4js from 'log4js'
+import * as learnerrecord from 'lib/learnerrecord'
 import * as template from 'lib/ui/template'
 
 export async function index(req: Request, res: Response) {
-	// const result = await catalog.listAll({})
-
-	res.send(template.render('bookings/index', req, {}))
+	const registrations = await learnerrecord.getRegistrations()
+	res.send(
+		template.render('bookings/index', req, {registrations: registrations})
+	)
 }
