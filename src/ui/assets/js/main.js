f225a0e91d48947f3e3131591b4ec63ffd281982
Jennifer Duong <jen@cautionyourblast.com>
2018-02-28 15:19:05 +0000

    removing not used code + fixing add js-enabled class

--- src/ui/assets/js/main.js
+++ src/ui/assets/js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-	$('body').addClass('js-enabled')
-	$(document).on('click', '[data-target="view-all-button"]', function(event) {
-		event.preventDefault()
-		$(this).toggleClass('view-all--showing')
-		$('#view-all').toggleClass('js-hidden')
-	})
 	// Install click handler to reveal the feedback form.
 	var feedbackRevealed = false
 	$('.feedback-prompt').click(function(e) {
