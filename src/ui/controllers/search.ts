493ec0b4aa1e5e52895d49ef1a035a90b6b66423
matthewmu <matthewmu@kainos.com>
2018-03-26 16:16:35 +0100

    Adding pagination

--- src/ui/controllers/search.ts
+++ src/ui/controllers/search.ts
@@ -4,19 +4,48 @@ import * as api from 'lib/service/catalog/api'
 import * as template from 'lib/ui/template'
 import * as striptags from 'striptags'
 
+function range(start: number, stop?: number, step?: number) {
+	const out = []
+	if (stop === undefined) {
+		stop = start
+		start = 0
+	}
+	if (step) {
+		while (stop > start) {
+			out.push(start)
+			start += step
+		}
+	} else {
+		while (stop > start) {
+			out.push(start)
+			start += 1
+		}
+	}
+	return out
+}
+
 export async function search(req: express.Request, res: express.Response) {
 	let query = ''
+	let page = 0
+	let size = 10
+
 	let searchResults: api.SearchResults = {
 		page: 0,
 		results: [],
-		size: 20,
+		size: 10,
 		totalResults: 0,
 	}
 	const start = new Date()
+	if (req.query.p){
+		page = req.query.p
+	}
+	if (req.query.p){
+		size = req.query.s
+	}
 	if (req.query.q) {
 		query = striptags(req.query.q)
-		searchResults = await catalog.search(query)
+		searchResults = await catalog.search(query, page, size)
 	}
 	const end: string = (((new Date() as any) - (start as any)) / 1000).toFixed(2)
-	res.send(template.render('search', req, res, {end, query, searchResults}))
+	res.send(template.render('search', req, res, {end, query, searchResults, range}))
 }
