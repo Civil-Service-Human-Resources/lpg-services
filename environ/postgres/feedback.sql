233dd80fa0e988eaea5876dbccc7dd74465d0f1f
tav <tav@espians.com>
2018-03-26 12:15:53 +0100

    environ: merge learning-locker back into single image and patch it

--- environ/postgres/feedback.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE feedback (
-  id               SERIAL PRIMARY KEY,
-  created          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  page_url         TEXT NOT NULL,
-  went_wrong       TEXT NOT NULL,
-  what_doing       TEXT NOT NULL
-);
