3783ab7b0b8c8cc753dba7f026502048ef926afe
Max Stewart <max@cautionyourblast.com>
2018-04-18 10:41:47 +0100

    Logging.

--- src/lib/ui/template.ts
+++ src/lib/ui/template.ts
@@ -137,10 +137,7 @@ function getCurrentRequest() {
 }
 
 function getName(ident: string) {
-	ident = ident
-		.split('/')
-		.pop()!
-		.replace(/-/g, '')
+	ident = ident.split('/').pop()!.replace(/-/g, '')
 	return ident.charAt(0).toUpperCase() + ident.slice(1)
 }
 
