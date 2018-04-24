f21400fa98fe230c893beccfe466d13c0a87f8f6
Jen <duong.jenniferm@gmail.com>
2018-04-24 11:34:17 +0100

    Update edit.ts

--- src/management-ui/controllers/module/edit.ts
+++ src/management-ui/controllers/module/edit.ts
@@ -208,7 +208,7 @@ export async function setModule(ireq: express.Request, res: express.Response) {
 				res.sendStatus(500)
 				return
 			}
-			// req.course.duration = duration
+			
 			data.duration = duration
 			data.title = data.title || info.title
 		}
