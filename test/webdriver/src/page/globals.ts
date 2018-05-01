7873ca4af77f710ad11de66e685dcc08eefcefb8
William Pritchard <williamp@kainos.com>
2018-05-01 14:53:18 +0100

    feedback tests

--- test/webdriver/src/page/globals.ts
+++ test/webdriver/src/page/globals.ts
@@ -2,6 +2,23 @@ export const selectors: Record<string, string> = {
 	author: 'a[href="#"]',
 	cookies: 'a[href="https://www.gov.uk/help/cookies"]',
 	copyright: '.copyright > a',
+	feedbackDetails: '.feedback-details',
+	feedbackDoingField: '#whatDoing',
+	feedbackPrompt: '.feedback-prompt',
+	feedbackSubmitButton: 'button[name="commit"]',
+	feedbackWrongField: '#wentWrong',
+	homeMenuButton: 'a[href="/home"]',
+	learningRecordMenuButton: 'a[href="/learning-record"]',
 	license: 'a[rel="license"]',
 	privacy: 'a[href="/privacy"]',
+	profileMenuButton: 'a[href="/profile"]',
+	searchMenuButton: 'a[href="/search"]',
+	signoutMenuButton: 'a[href="/sign-out"]',
+	suggestionsMenuButton: 'a[href="suggestions-for-you"]',
+}
+
+export function completeFeedback() {
+	browser.setValue(selectors.feedbackDoingField, 'What you were doing')
+	browser.setValue(selectors.feedbackWrongField, 'What went wrong')
+	browser.click(selectors.feedbackSubmitButton)
 }
