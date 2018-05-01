728796813cce94d0ce1535349b8f6e5fba2c46f2
William Pritchard <williamp@kainos.com>
2018-05-01 14:17:02 +0100

    footer tests

---
+++ test/webdriver/src/test/footer.spec.ts
@@ -0,0 +1,46 @@
+import * as config from '../extension/config'
+import {selectors} from '../page/globals'
+import loginPage from '../page/login'
+
+describe('Footer for LPG', () => {
+	beforeAll(done => {
+		browser.url(config.URL)
+	})
+
+	it('Display footer author', () => {
+		expect(browser.isVisible(selectors.author)).toBe(true)
+	})
+	it('Display footer cookies', () => {
+		expect(browser.isVisible(selectors.cookies)).toBe(true)
+	})
+	it('Display footer copywrite', () => {
+		expect(browser.isVisible(selectors.copyright)).toBe(true)
+	})
+	it('Display footer license', () => {
+		expect(browser.element(selectors.license).isVisible()).toBe(true)
+	})
+	it('Display footer privacy', () => {
+		expect(browser.isVisible(selectors.privacy)).toBe(true)
+	})
+
+	it('Display footer author logged in', () => {
+		loginPage.testAccountLogin()
+		expect(browser.isVisible(selectors.author)).toBe(true)
+	})
+
+	it('Display footer cookies logged in', () => {
+		expect(browser.isVisible(selectors.cookies)).toBe(true)
+	})
+
+	it('Display footer copywrite logged in', () => {
+		expect(browser.isVisible(selectors.copyright)).toBe(true)
+	})
+
+	it('Display footer license logged in', () => {
+		expect(browser.element(selectors.license).isVisible()).toBe(true)
+	})
+
+	it('Display footer privacy logged in', () => {
+		expect(browser.isVisible(selectors.privacy)).toBe(true)
+	})
+})
