e3e066ff1ffb0c6b4cd8f1546fd64c5c715bceca
Jennifer Duong <jen@cautionyourblast.com>
2018-05-11 13:50:52 +0100

    LPFG-288

--- src/ui/controllers/user.ts
+++ src/ui/controllers/user.ts
@@ -139,22 +139,38 @@ export interface Level {
 	name: string
 }
 
-export interface RegistryRole {
-	name: string
-	_links: {
-		self: {href: string}
-		jobRole: {href: string}
-		parent: {href: string}
-		children: {href: string}
-		profession: {href: string}
+export function parseProfessions(
+	traversonResult: any
+): [Level[], string] | void {
+	try {
+		const parsed = traversonResult._embedded.professions.map(
+			(profession: any) => {
+				return {
+					name: profession.name,
+					url: profession._links.jobRoles.href,
+				}
+			}
+		)
+		return [parsed, traversonResult._links.self.href]
+	} catch (e) {
+		logger.error(e)
+		return
 	}
 }
 
-export interface RegistryProfession {
-	name: string
-	_links: {
-		self: {href: string}
-		jobRoles: {href: string}
+export function parseRoles(traversonResult: any): [Level[], string] | void {
+	try {
+		const parsed = traversonResult._embedded.jobRoles.map((role: any) => {
+			return {
+				hasChildren: role.hasChildren,
+				name: role.name,
+				url: role._links.href,
+			}
+		})
+		return [parsed, traversonResult._links.self.href]
+	} catch (e) {
+		logger.error(e)
+		return
 	}
 }
 
@@ -166,78 +182,85 @@ export async function newRenderAreasOfWorkPage(
 	let selectedArr = []
 	let currentLevel
 	let selected
-	let hasChildren: boolean = true
 	let levels: Level[][] = []
 	let prevLevelUrl
 
 	if (req.query.select) {
-		//update method goes here
+		//TODO: update method goes here
 		res.redirect('/profile')
 	}
 
 	if (req.params[0]) {
+		/* set the 'progress' vars */
 		selectedArr = req.params[0].split('/')
 		currentLevel = selectedArr.length
 		selected = selectedArr[currentLevel - 1]
 	}
 
 	if (selectedArr.length === 0) {
+		/* if the user hasn't selected anything, start from the beginning and reset 'levels' vars */
 		req.session!.levels = []
 		levels = []
-		//if there are no levels selected
+
 		const traversonResult = await traverson
 			.from(`${config.REGISTRY_SERVICE_URL}/professions`)
 			.jsonHal()
 			.getResource().result
-		try {
-			const parsed = traversonResult._embedded.professions.map(
-				(profession: any) => {
-					return {
-						name: profession.name,
-						url: profession._links.jobRoles.href,
-					}
-				}
-			)
-			req.session!.prevLevelUrl = traversonResult._links.self.href
-			console.log(req.session!.prevLevelUrl)
-			levels.push(parsed)
-			req.session!.levels = levels
-		} catch (e) {
-			logger.error(e)
+
+		const parsed = parseProfessions(traversonResult)
+		if (parsed) {
+			levels.push(parsed[0])
+			req.session!.prevLevelUrl = parsed[1]
 		}
 	} else {
+		/* if the user has selected levels or there are req.params[0] */
 		levels = req.session!.levels
 		const followPath: string[] = []
 		prevLevelUrl = req.session!.prevLevelUrl
 
-		if (selectedArr) {
-			if (selectedArr.length === 1) {
-				followPath.push(`professions[${selected}]`, 'jobRoles')
-			} else {
-				followPath.push(`jobRoles[${selected}]`, 'children')
-			}
+		if (levels.length === selectedArr.length - 1) {
+			/* check the session. If the amount of levels saved matches up with the amount of selections made */
+			selectedArr.length === 1
+				? followPath.push(`professions[${selected}]`, 'jobRoles')
+				: followPath.push(`jobRoles[${selected}]`, 'children')
+		} else {
+			/* If they don't match up, start from the beginning and reconstruct the path.
+			 * This may happen when they use the url to navigate to a level
+			 */
+			prevLevelUrl = `${config.REGISTRY_SERVICE_URL}/professions`
+			selectedArr.forEach((selection: number, index: number) => {
+				index === 0
+					? followPath.push(`professions[${selection}]`, 'jobRoles')
+					: followPath.push(`jobRoles[${selection}]`, 'children')
+			})
 		}
+
 		const traversonResult = await traverson
 			.from(prevLevelUrl)
 			.jsonHal()
 			.follow(followPath)
 			.getResource().result
-		try {
-			const parsed = traversonResult._embedded.jobRoles.map((role: any) => {
-				return {
-					name: role.name,
-					url: role._links.href,
-				}
-			})
 
-			req.session!.prevLevelUrl = traversonResult._links.self.href
-
-			levels[selectedArr.length] = parsed
-		} catch (e) {
-			logger.error(e)
+		if (levels.length === 0) {
+			/* if there are no levels saved, use parseProfessions method since the response contains 'professions' key */
+			const parsed = parseProfessions(traversonResult)
+			if (parsed) {
+				levels.push(parsed[0])
+				req.session!.prevLevelUrl = parsed[1]
+			}
+		} else {
+			/* if there are levels saved, use parseRoles method since the response contains 'jobRoles' key */
+			const parsed = parseRoles(traversonResult)
+			if (parsed) {
+				/* only set the results to the appropriate level*/
+				levels[selectedArr.length] = parsed[0]
+				req.session!.prevLevelUrl = traversonResult._links.self.href
+			}
 		}
 	}
+
 	if (selectedArr) {
+		/* Slice the array to the amount of levels the user should see and save the levels to session */
 		levels = levels.slice(0, selectedArr.length + 1)
 		req.session!.levels = levels
 	}
@@ -246,7 +269,6 @@ export async function newRenderAreasOfWorkPage(
 		template.render('profile/edit', req, res, {
 			currentLevel,
 			inputName: 'areas-of-work',
-			hasChildren,
 			lede,
 			levels,
 			...res.locals,
