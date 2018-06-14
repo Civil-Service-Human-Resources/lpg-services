c01caae1758feb2d01f65010f5fb869c8f0a204f
Jennifer Duong <jen@cautionyourblast.com>
2018-06-14 16:56:09 +0100

    LPFG-372 LPFG-373 error handling (+1 squashed commit) Squashed commits: [1399410] LPFG-372 LPFG-373

--- src/lib/service/notify.ts
+++ src/lib/service/notify.ts
@@ -2,10 +2,17 @@ import * as config from 'lib/config'
 import * as gov from 'notifications-node-client'
 
 export interface BookingCancellation {
+	bookingReference: string
 	courseDate: string
 	courseTitle: string
+	cost: number | undefined
 	email: string
-	name: string
+	learnerName: string
+	courseLocation: string
+	lineManager: {
+		email: string
+		name: string
+	}
 }
 
 export interface BookingConfirmation {
@@ -28,25 +35,42 @@ export interface BookingConfirmation {
 
 export async function bookingCancelled(info: BookingCancellation) {
 	const notify = new gov.NotifyClient(config.GOV_NOTIFY_API_KEY)
-	for (const recipient of config.BOOKING_NOTIFY_RECIPIENTS) {
-		const resp = await notify.sendEmail(
-			config.BOOKING_NOTIFY_TEMPLATE_IDS.cancelled,
-			recipient,
-			{personalisation: info}
-		)
-		if (resp.statusCode !== 201) {
+
+	const templateData = {...info}
+
+	await notify
+		.sendEmail(config.BOOKING_NOTIFY_TEMPLATE_IDS.cancelled, info.email, {
+			personalisation: templateData,
+		})
+		.catch(reason => {
 			throw new Error(
-				`Got unexpected response status ${
-					resp.statusCode
-				} when posting booking cancellation to GOV Notify`
+				`Got unexpected response status ${reason} when posting booking confirmation to GOV Notify`
 			)
-		}
+		})
+
+	if (info.lineManager) {
+		await notify
+			.sendEmail(
+				config.BOOKING_NOTIFY_TEMPLATE_IDS.cancelledLineManager,
+				info.lineManager.email,
+				{
+					personalisation: {
+						...templateData,
+						recipient: info.lineManager.name || info.lineManager.email,
+					},
+				}
+			)
+			.catch(reason => {
+				throw new Error(
+					`Got unexpected response status ${reason} when posting booking confirmation to GOV Notify`
+				)
+			})
 	}
 }
 
 export async function bookingRequested(info: BookingConfirmation) {
 	const notify = new gov.NotifyClient(config.GOV_NOTIFY_API_KEY)
-	const templateData = {...info, lineManager: 'jen@cyb.digital'}
+	const templateData = {...info}
 
 	await notify
 		.sendEmail(config.BOOKING_NOTIFY_TEMPLATE_IDS.confirmed, info.email, {
