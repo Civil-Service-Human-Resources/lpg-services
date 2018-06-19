0ce6d2e597aa0dd4566992d889fea375d52c2c34
Jennifer Duong <jen@cautionyourblast.com>
2018-06-19 16:20:11 +0100

    LPFG-302, LPFG-426, chore/call-to-actions fix imports (+5 squashed commits) Squashed commits: [b6496d2] moving courseCallToAction and linting [b03a433] fix tests [ab8f825] more logic [12022f3] LPFG-426 adjust test flow. use returned attributes (+8 squashed commits) Squashed commits: [00f473f] wip [aed3a7b] wip [9db2a75] module tests done [81a9d45] more logic [aaf6bb9] rename and functionality [2d08065] tests [4bf291d] call to action modules outline [b21848a] cherry-pick lpfg-426 [c5b410e] delete old (+12 squashed commits) Squashed commits: [8ea64ea] linting [0a8c385] linting [68f79ee] cleaning up tests [88dd894] tests [3b1104e] more tests [bf97c7b] LPFG-more tests [cae807f] testing [38004d9] skip link [5904799] actions to record [5a9afaa] clean up [7b19c3f] LPFG-wip [e88e06a] wip

---
+++ src/lib/ui/courseCallToAction.ts
@@ -0,0 +1,113 @@
+import * as model from 'lib/model'
+
+export enum CourseActionType {
+	Add = 'Add',
+	Delete = 'Delete',
+}
+export interface ActionToPlan {
+	type: CourseActionType
+	url: string
+}
+
+export interface CallToActionProps {
+	accessibilityHelper: string
+	actionToPlan?: ActionToPlan
+	actionToRecord?: {
+		move: string
+		skip: string
+	}
+	isInLearningPlan: boolean
+	message: string
+	url: string
+}
+
+export function constructCourseCallToAction(
+	course: model.Course,
+	user: model.User,
+	modifier?: string
+) {
+	const courseType = course.getType()
+	const isRequired: boolean = course.isRequired(user)
+	const isHome: boolean = modifier === 'home'
+
+	const callToActionProps: CallToActionProps = {
+		accessibilityHelper: ' this course',
+		isInLearningPlan: false,
+		message:
+			courseType === 'face-to-face' ? 'action_BOOK' : 'action_NOT_STARTED',
+		url:
+			courseType === 'face-to-face'
+				? `/book/${course.id}/${course.modules[0].id}/choose-date`
+				: `/courses/${course.id}#modules`,
+	}
+
+	if (isRequired) {
+		callToActionProps.isInLearningPlan = true
+	}
+
+	if (courseType === 'face-to-face') {
+		callToActionProps.url = `/book/${course.id}/${
+			course.modules[0].id
+		}/choose-date`
+		callToActionProps.message = 'action_BOOK'
+	}
+
+	if (course.record) {
+		const record = course.record
+		callToActionProps.isInLearningPlan = true
+		switch (courseType) {
+			case 'face-to-face':
+				const hasEvent =
+					record.modules && record.modules.length && record.modules[0].eventId
+
+				const isBooked = hasEvent && record.modules[0].state === 'REGISTERED'
+				const isCancelled =
+					hasEvent && record.modules[0].state === 'UNREGISTERED'
+
+				const isDatePassed = new Date() > course.getSelectedDate()!
+
+				if (hasEvent) {
+					if (isBooked) {
+						if (isDatePassed) {
+							callToActionProps.actionToRecord = {
+								move: `/home?move=${course.id},${
+									course.record.modules[0].moduleId
+								},${course.record.modules[0].eventId}`,
+								skip: `/home?skip=${course.id},${
+									course.record.modules[0].moduleId
+								},${course.record.modules[0].eventId}`,
+							}
+							delete callToActionProps.url
+							delete callToActionProps.message
+						} else {
+							callToActionProps.url = `/book/${course.id}/${
+								record.modules[0].moduleId
+							}/${record.modules[0].eventId}/cancel`
+							callToActionProps.message = `action_CANCEL`
+						}
+					}
+					if (isCancelled) {
+						delete callToActionProps.url
+						delete callToActionProps.message
+					}
+				}
+				break
+			default:
+				break
+		}
+		if (!isRequired && record.state !== 'REGISTERED' && isHome) {
+			callToActionProps.actionToPlan = {
+				type: CourseActionType.Delete,
+				url: `/home?delete=${course.id}`,
+			}
+		}
+	} else {
+		if (!isRequired && modifier === 'search') {
+			callToActionProps.actionToPlan = {
+				type: CourseActionType.Add,
+				url: `/suggestions-for-you/add/${course.id}?ref=${modifier}`,
+			}
+		}
+	}
+	return callToActionProps
+}
