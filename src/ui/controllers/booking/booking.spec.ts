1347cad211ead094a7392f3cd028f92c0c9db60a
Max Stewart <max@cautionyourblast.com>
2018-06-25 15:43:41 +0100

    Drive payment options off CSRS data and department

--- src/ui/controllers/booking/booking.spec.ts
+++ src/ui/controllers/booking/booking.spec.ts
@@ -1,22 +1,40 @@
 import {expect} from 'chai'
-import {validatePurchaseOrder} from './booking'
+import {validate} from './booking'
 
-describe('Purchase Order validation', () => {
-	it('should not return any errors for a correct PO', () => {
-		expect(validatePurchaseOrder('AB12345')).to.eql([])
+describe('Payment validation', () => {
+
+	describe('of type PURCHASE_ORDER', () => {
+
+		const validatePO = (value: string) => validate('PURCHASE_ORDER', value)
+
+		it('should not return any errors for a correct PO', () => {
+			expect(validatePO('AB12345')).to.eql([])
+		})
+
+		it('should only return errors.po-empty', () => {
+			expect(validatePO('')).to.eql(['errors.po-empty'])
+		})
+
+		it('should only return errors.po-too-short', () => {
+			expect(validatePO('1')).to.eql(['errors.po-too-short'])
+		})
+
+		it('should return errors.po-special-characters AND errors.po-too-long', () => {
+			expect(validatePO(':this,is#an:invalidPOthatsWayTooLong')
+			).to.eql(['errors.po-too-long', 'errors.po-special-characters'])
+		})
 	})
 
-	it('should only return errors.po-empty', () => {
-		expect(validatePurchaseOrder('')).to.eql(['errors.po-empty'])
-	})
+	describe('of type FINANCIAL_APPROVER', () => {
 
-	it('should only return errors.po-too-short', () => {
-		expect(validatePurchaseOrder('1')).to.eql(['errors.po-too-short'])
-	})
+		const validateFAP = (value: string) => validate('FINANCIAL_APPROVER', value)
 
-	it('should return errors.po-special-characters AND errors.po-too-long', () => {
-		expect(
-			validatePurchaseOrder(':this,is#an:invalidPOthatsWayTooLong')
-		).to.eql(['errors.po-too-long', 'errors.po-special-characters'])
+		it('should not return any errors for a correct value', () => {
+			expect(validateFAP('learner@domain.com')).to.eql([])
+		})
+
+		it('should return error for incorrect value', () => {
+			expect(validateFAP('domain.com')).to.eql(['errors.invalid-email-address'])
+		})
 	})
 })
