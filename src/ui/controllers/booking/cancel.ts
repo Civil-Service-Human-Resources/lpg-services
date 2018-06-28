583d7b27676d582d7a940a143de7bf117406b8a3
Alan <alan.work@teamsmog.com>
2018-06-28 16:51:50 +0100

    save reason as extension
    utility
    
    utility

--- src/ui/controllers/booking/cancel.ts
+++ src/ui/controllers/booking/cancel.ts
@@ -29,15 +29,32 @@ export async function renderCancelBookingPage(
 
 	course.record = record!
 
+	const moduleRecord = record!.modules.find(
+		rm => rm.moduleId === module.id && rm.eventId === event.id
+	)
+
+	if (!moduleRecord) {
+		res.sendStatus(400)
+		return
+	}
+
+	(module as any).record = moduleRecord
+
+	const optionType = 'radio'
+	const options = Object.entries(req.__('cancelReasons'))
+
 	res.send(
 		template.render('booking/cancel-booking', req, res, {
 			course,
 			error: req.flash('cancelBookingError')[0],
 			event,
 			module,
+			optionType,
+			options,
 		})
 	)
 }
+
 
 export async function renderCancelledBookingPage(
 	ireq: express.Request,
@@ -105,16 +122,26 @@ export async function tryCancelBooking(
 
 	course.record = record
 
-	if (req.body['cancel-tc']) {
-		await xapi
-			.record(req, course, xapi.Verb.Unregistered, undefined, module, event)
-			.catch((error: any) => {
-				req.session!.save(() => {
-					req.flash('cancelBookingError', error.message)
-					res.redirect(`/book/${course.id}/${module.id}/${event.id}/cancel`)
-				})
-			})
+	const extensions: Record<string, any> = {}
+	const cancelReason = req.body['other-reason'] ? req.body['cancel-reason'] : req.body['cancel-reason']
 
+	if (cancelReason) {
+		extensions[xapi.Extension.CancelReason] = cancelReason
+	}
+	console.log(1)
+	const errors = await xapi
+		.record(req, course, xapi.Verb.Unregistered, extensions, module, event)
+		.catch((error: any) => {
+			req.session!.save(() => {
+				req.flash('cancelBookingError', error.message)
+				res.redirect(`/book/${course.id}/${module.id}/${event.id}/cancel`)
+				console.log(error.message)
+				return true
+			})
+		})
+	console.log(3)
+	if (!errors) {
+		console.log(4)
 		await notify.bookingCancelled({
 			bookingReference: `${req.user.id}-${event.id}`,
 			cost: module.price,
@@ -132,12 +159,10 @@ export async function tryCancelBooking(
 			learnerName: req.user.givenName || req.user.userName,
 			lineManager: req.user.lineManager,
 		})
+
 		req.session!.save(() => {
+			console.log(5)
 			res.redirect(`/book/${course.id}/${module.id}/${event.id}/cancelled`)
-		})
-	} else {
-		req.session!.save(() => {
-			res.redirect(`/book/${course.id}/${module.id}/${event.id}/cancel`)
 		})
 	}
 }
