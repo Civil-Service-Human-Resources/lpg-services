de638963acaccce744340f14ad28494ecd0d1165
Max Stewart <max@cautionyourblast.com>
2018-07-09 12:35:59 +0100

    Resolve rebase issue.

--- src/ui/controllers/booking/booking.ts
+++ src/ui/controllers/booking/booking.ts
@@ -218,7 +218,7 @@ export async function renderOuch(
 	)
 }
 
-export function renderPaymentOptions(
+export async function renderPaymentOptions(
 	req: express.Request,
 	res: express.Response
 ) {
