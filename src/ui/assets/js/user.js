71e27c3e11bfb773c0708f0842d2f74522bdfd80
Max Stewart <max@cautionyourblast.com>
2018-08-09 16:52:51 +0100

    Override close function in storyline package.

--- src/ui/assets/js/user.js
+++ src/ui/assets/js/user.js
@@ -8,6 +8,62 @@ if (typeof XMLHttpRequest !== 'undefined') {
   }
 }
 
+
+var CLOSE_METHODS = {
+
+  csl: function() {
+    var match = window.location.toString().match(/(https?):\/\/([^-]*)-?cdn\.cshr\.digital\/.*\/([^/]+)\/([^/]+)\/.*$/);
+    if (!match) {
+      throw new Error('Content being accessed on invalid domain');
+    }
+    var scheme = match[1];
+    var env = !!match[2] ? match[2] + '-' : '';
+    var host = env + 'lpg.' +'cshr.digital/';
+    var path = 'learning-record/' + match[3] + '/' + match[4];
+
+    if (match[2] === 'local') {
+      scheme = 'http';
+      host = 'lpg.local.cshr.digital:3001/';
+    }
+
+    window.location = scheme + '://' + host + path;
+    return true;
+  }
+};
+
+function s() {
+  var pubSub;
+  try {
+    if (window.require && !!(pubSub = require('helpers/pubSub'))) {
+      pubSub.on('player:closing', CLOSE_METHODS['csl']);
+      return;
+    }
+  } catch(e) {
+  }
+  setTimeout(s, 1);
+}
+s();
+
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+// Native js method to run on document ready thats crossbrowser and also will not override any existing on ready code
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+r(function(){
+  var title = getParameterByName('title');
+  document.title = title;
+});
+
+
 if (false) {
   // Code that enables debug output from TinCan lib.
   function d() {
@@ -19,3 +75,4 @@ if (false) {
   }
   d();
 }
+
