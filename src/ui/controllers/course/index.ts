565b3feea1abceef2fb48b5496f206b653657f87
Max Stewart <max@cautionyourblast.com>
2018-08-13 15:28:13 +0100

    Provide empty actor in URL params for xAPI package code.

--- src/ui/controllers/course/index.ts
+++ src/ui/controllers/course/index.ts
@@ -94,7 +94,7 @@ export async function displayModule(
 				`${config.CONTENT_URL}/${course.id}/${module.id}/${
 					module.startPage
 				}?title=${module.title || course.title}&endpoint=${config.LPG_UI_SERVER}/courses/${
-					course.id}/${module.id}/xapi/&actor={}`
+					course.id}/${module.id}/xapi/&actor={"name":"Noop"}`
 			)
 			break
 		case 'face-to-face':
