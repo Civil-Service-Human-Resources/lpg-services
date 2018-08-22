7136ffe247fafb78061cbf2ef79621062b6713ac
Peter Taylor <petertaylor@Malekith.local>
2018-08-22 16:14:35 +0100

    LPFG-656: Fixed space issues

--- src/management-ui/server.ts
+++ src/management-ui/server.ts
@@ -208,9 +208,9 @@ app.use(
 )
 
 app.use(function(req, res, next) {
-    res.locals.success_messages = req.flash('success_messages')
-    res.locals.error_messages = req.flash('error_messages')
-    next()
+	res.locals.success_messages = req.flash('success_messages')
+	res.locals.error_messages = req.flash('error_messages')
+	next()
 })
 
 app.listen(PORT, () => {
