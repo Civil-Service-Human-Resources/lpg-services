e9709fa9af69d901f0f74ccb8977fc4de68bff3d
Peter Taylor <petertaylor@Malekith.local>
2018-08-22 16:20:45 +0100

    LPFG-656: Removing erroneous function

--- src/management-ui/server.ts
+++ src/management-ui/server.ts
@@ -207,12 +207,6 @@ app.use(
 	}
 )
 
-app.use(function(req, res, next) {
-	res.locals.success_messages = req.flash('success_messages')
-	res.locals.error_messages = req.flash('error_messages')
-	next()
-})
-
 app.listen(PORT, () => {
 	logger.info(`listening on port ${PORT}`)
 })
