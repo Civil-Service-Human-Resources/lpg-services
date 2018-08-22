0d1231763bb70d6fe9843019272a0c4e4e02578f
Peter Taylor <petertaylor@Malekith.local>
2018-08-22 16:48:23 +0100

    LPFG-656: Making travis ci re run

--- src/ui/controllers/course/index.ts
+++ src/ui/controllers/course/index.ts
@@ -121,7 +121,7 @@ export async function displayModule(
 					sessionId,
 					video: module.url!.endsWith('.mp4')
 						? null
-						: await youtube.getBasicInfo(module.url!)
+						: await youtube.getBasicInfo(module.url!),
 				})
 			)
 			break
