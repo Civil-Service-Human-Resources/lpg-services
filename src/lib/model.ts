d9bf5ad6a7558fcc66d80859a8344bedc1ef3b35
Peter Taylor <petertaylor@Malekith.local>
2018-10-02 14:48:40 +0100

    fix/LPFG-799: Added whitespace

--- src/lib/model.ts
+++ src/lib/model.ts
@@ -18,7 +18,7 @@ export class Course {
 		course.modules = (data.modules || []).map(Module.create)
 
 		let audiences = []
-		if(data.course && data.course.audiences) {
+		if (data.course && data.course.audiences) {
 			audiences = (data.course.audiences).map(Audience.create)
 		}
 
