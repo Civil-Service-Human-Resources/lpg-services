0d5386610fd95f2fdb5bd13dbb864e31713efcb8
Peter Taylor <petertaylor@Malekith.local>
2018-10-16 15:31:01 +0100

    Removed space

--- src/lib/model.ts
+++ src/lib/model.ts
@@ -18,7 +18,7 @@ export class Course {
 		course.modules = (data.modules || []).map(Module.create)
 
 		const audiences = (data.audiences || []).map(Audience.create)
-		course.audiences =  audiences
+		course.audiences = audiences
 
 		if (user) {
 			let matchedAudience = null
