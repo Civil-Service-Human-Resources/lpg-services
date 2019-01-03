a137b1965515fcd625962a5e6bbc82bddacb79ee
Rob Leney <roble@kainos.com>
2019-01-03 14:24:25 +0000

    LPFG-907: Added endpoints to update profession, other areas of work, interests and grade in profile.

--- src/ui/controllers/profile.ts
+++ src/ui/controllers/profile.ts
@@ -39,7 +39,7 @@ export async function updateOrganisation(request: Request, response: Response) {
 	if (!organisation) {
 		response.send(template.render('profile/organisation', request, response, {
 			error: true,
-			organisation: organisation,
+			organisation,
 		}))
 	}
 
@@ -51,6 +51,96 @@ export async function updateOrganisation(request: Request, response: Response) {
 		throw new Error(error)
 	}
 	response.send(template.render('profile/organisation', request, response, {
-		name: organisation,
+		organisation,
+	}))
+}
+
+export async function addProfession(request: Request, response: Response) {
+	const professions = await registry.halNode("professions")
+	response.send(template.render('profile/profession', request, response, {
+		professions
+	}))
+}
+
+export async function updateProfession(request: Request, response: Response) {
+	const profession = request.body.profession
+
+	try {
+		await registry.patch('civilServants', {
+			profession,
+		}, request.user.accessToken)
+	} catch (error) {
+		throw new Error(error)
+	}
+	response.send(template.render('profile/profession', request, response, {
+		profession,
+	}))
+}
+
+export async function addOtherAreasOfWork(request: Request, response: Response) {
+	const professions = await registry.halNode("professions")
+	response.send(template.render('profile/otherAreasOfWork', request, response, {
+		professions
+	}))
+}
+
+export async function updateOtherAreasOfWork(request: Request, response: Response) {
+	const otherAreasOfWork = request.body.otherAreasOfWork
+
+	try {
+		await registry.patch('civilServants', {
+			otherAreasOfWork
+		}, request.user.accessToken)
+	} catch (error) {
+		throw new Error(error)
+	}
+
+	response.send(template.render('profile/otherAreasOfWork', request, response, {
+		otherAreasOfWork
+	}))
+}
+
+export async function addInterests(request: Request, response: Response) {
+	const interests = await registry.halNode("interests")
+	response.send(template.render('profile/interests', request, response, {
+		interests
+	}))
+}
+
+export async function updateInterests(request: Request, response: Response) {
+	const interests = request.body.interests
+
+	try {
+		await registry.patch('civilServants', {
+			interests
+		}, request.user.accessToken)
+	} catch (error) {
+		throw new Error(error)
+	}
+
+	response.send(template.render('profile/interests', request, response, {
+		interests
+	}))
+}
+
+export async function addGrade(request: Request, response: Response) {
+	const grades = await registry.halNode("grades")
+	response.send(template.render('profile/grade', request, response, {
+		grades
+	}))
+}
+
+export async function updateGrade(request: Request, response: Response) {
+	const grade = request.body.grade
+
+	try {
+		await registry.patch('civilServants', {
+			grade,
+		}, request.user.accessToken)
+	} catch (error) {
+		throw new Error(error)
+	}
+	response.send(template.render('profile/grade', request, response, {
+		grade,
 	}))
 }
