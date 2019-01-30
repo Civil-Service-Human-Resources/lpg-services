e0a2110c60df0fd2cc859dbf52089ee7cf5bb99a
Michael Jones <michaelj@kainos.com>
2019-01-30 11:08:53 +0000

    fixed skill index.ts

--- src/ui/controllers/skills/index.ts
+++ src/ui/controllers/skills/index.ts
@@ -4,7 +4,7 @@ import * as learnerRecord from 'lib/learnerrecord'
 import * as model from 'lib/model'
 import * as template from 'lib/ui/template'
 import * as log4js from 'log4js'
-import * as path from 'path';
+import * as path from 'path'
 import * as suggestionController from '../suggestion'
 const logger = log4js.getLogger('controllers/skills/')
 
@@ -43,23 +43,31 @@ export async function startQuiz(req: express.Request, res: express.Response) {
 }
 
 export async function chooseQuiz(req: express.Request, res: express.Response) {
+	res.send(template.render('skills/choose-quiz', req, res))
+}
+
+export async function questions(req: express.Request, res: express.Response) {
 	const numberOfQuestions = 10
-	const jsonString = JSON.parse(fs.readFileSync(path.join(__dirname, '../../../../../src/questions.json'), 'utf8'))
+	const jsonString = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, '../../../../../src/questions.json'),
+			'utf8'
+		)
+	)
 	const questionsSet: any = []
 	let count = 1
 	while (questionsSet.length < numberOfQuestions) {
-		const randomQuestion = jsonString[Math.floor(Math.random() * jsonString.length)]
+		const randomQuestion =
+			jsonString[Math.floor(Math.random() * jsonString.length)]
 		if (!questionsSet.includes(randomQuestion)) {
 			questionsSet.push({questionNumber: count, question: randomQuestion})
 			count++
 		}
 	}
 	req.session!.questions = [questionsSet]
-	res.send(template.render('skills/choose-quiz', req, res, {results: questionsSet}))
-}
-
-export async function questions(req: express.Request, res: express.Response) {
-	res.send(template.render('skills/questions', req, res, {}))
+	res.send(
+		template.render('skills/questions', req, res, {results: questionsSet})
+	)
 }
 
 export async function quizSummary(req: express.Request, res: express.Response) {
