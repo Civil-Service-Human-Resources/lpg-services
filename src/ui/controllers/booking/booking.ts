1b2d25c314821afb3c8ec9a5e3717e51a81f06d6
matthewmu <matthewmu@kainos.com>
2019-02-19 11:17:01 +0000

    LPFG-1186-MI-Accesability-Options-v2

--- src/ui/controllers/booking/booking.ts
+++ src/ui/controllers/booking/booking.ts
@@ -1,12 +1,10 @@
 import * as express from 'express'
 import * as config from 'lib/config'
-import * as dateTime from 'lib/datetime'
 import * as extended from 'lib/extended'
 import * as learnerRecord from 'lib/learnerrecord'
 import * as model from 'lib/model'
 import * as purchaseOrdersService from 'lib/purchase-orders'
 import * as registry from 'lib/registry'
-import * as notify from 'lib/service/notify'
 import * as template from 'lib/ui/template'
 import * as xapi from 'lib/xapi'
 
@@ -471,15 +469,30 @@ export async function tryCompleteBooking(
 	const module = req.module!
 	const event = req.event!
 	const session = req.session!
-	const paymentOption = `${session.payment.type}: ${session.payment.value}`
 
+	const accessibilityArray: string[] = []
+	for (const i in session.accessibilityReqs) {
+		if (i) {
+			const requirement = session.accessibilityReqs[i]
+			if (requirement === 'other') {
+				accessibilityArray.push(session.otherAccessibilityReqs)
+			} else {
+				accessibilityArray.push(
+					res.__(`accessibility-requirements`)[requirement]
+				)
+			}
+		}
+	}
+
+	const accessibilityOptions = accessibilityArray.join(', ')
 	const response = await learnerRecord.bookEvent(
 		course,
 		module,
 		event,
 		req.user,
 		req.session!.purchaseOrder,
-		session.payment.value
+		session.payment.value,
+		accessibilityOptions
 	)
 
 	let message
@@ -491,50 +504,6 @@ export async function tryCompleteBooking(
 			`event: ${event.id}`,
 			`response: ${response.status}`
 		)
-
-		const accessibilityArray: string[] = []
-		for (const i in session.accessibilityReqs) {
-			if (i) {
-				const requirement = session.accessibilityReqs[i]
-				if (requirement === 'other') {
-					accessibilityArray.push('Other')
-				} else {
-					accessibilityArray.push(
-						res.__(`accessibility-requirements`)[requirement]
-					)
-				}
-			}
-		}
-
-		if (module.cost !== 0) {
-			await notify
-				.bookingRequested({
-					accessibility: accessibilityArray.join(', '),
-					bookingReference: `${req.user.id}-${event.id}`,
-					cost: module.cost,
-					courseDate: `${dateTime.formatDate(event.date)} ${dateTime.formatTime(
-						event.date,
-						true
-					)} ${
-						module.duration
-							? 'to ' + dateTime.addSeconds(event.date, module.duration, true)
-							: ''
-					}`,
-					courseLocation: event.location,
-					courseTitle: module.title || course.title,
-					email: req.user.userName,
-					eventId: event.id,
-					learnerName: req.user.givenName || req.user.userName,
-					lineManager: req.user.lineManager,
-					location: event.location,
-					paymentOption,
-				})
-				.catch((e: Error) => {
-					logger.error('There was an error with GOV Notify', e)
-					res.redirect('/book/ouch')
-					return true
-				})
-		}
 
 		message = confirmedMessage.Booked
 	} else {
