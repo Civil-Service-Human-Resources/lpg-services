a9e09affb9f86be5660c4a21fe685a7cf08731c0
Yuksel Kurtbas <yukselk@kainos.com>
2019-05-17 15:28:24 +0100

    questions and summary

--- src/ui/controllers/skills/index.ts
+++ src/ui/controllers/skills/index.ts
@@ -1,14 +1,13 @@
 import * as express from 'express'
 import * as model from 'lib/model'
 import * as skillsApi from "lib/service/skills"
+import {Choice, Question} from "lib/service/skills/api";
 import * as template from 'lib/ui/template'
 
 export function introduction(req: express.Request, res: express.Response) {
-	//const user = req.user as model.User
+	const user = req.user as model.User
 	// @ts-ignore
-	//let show  = user.areasOfWork[0] === 22 || user.areasOfWork[1].toLocaleLowerCase() === 'policy';
-	// TODO: Remove after testing
-	const show = true
+	const show  = user.areasOfWork[0] === 22 || user.areasOfWork[1].toLocaleLowerCase() === 'policy';
 	res.send(template.render('skills/introduction', req, res, {show}))
 }
 
@@ -19,34 +18,74 @@ export function chooseQuiz(req: express.Request, res: express.Response) {
 export async function startQuiz(req: express.Request, res: express.Response) {
 	const user = req.user as model.User
 	// @ts-ignore
-	const quiz = await skillsApi.searchQuiz(user.areasOfWork[0], req.params.limit)
-
-	quiz.questionCount = quiz.questions.length
-	quiz.answers = []
-
-	req.session!.quiz = quiz
-	res.redirect(`skills/questions/${quiz.questionIndex}`)
+	req.session!.quiz = await skillsApi.searchQuiz(user.areasOfWork[0], req.body.limit)
+	res.redirect('/skills/questions/0')
 }
 
 export async function nextQuestion(req: express.Request, res: express.Response) {
-	const index = req.params.questionIndex
+	const index: number = parseInt(req.params.questionIndex, 0)
+	if (index > req.session!.quiz.questionCount - 1) {
+		res.redirect(`/skills/summary`)
+	}
 	const question = req.session!.quiz.questions[index]
-	const count =  req.session!.quiz.questionCount
+	const count = req.session!.quiz.questionCount
+	const type = getType(question.type)
 
 	res.send(
-		template.render('skills/questions/', req, res, {index, question, count})
+		template.render('skills/questions', req, res, {index, question, count, type})
 	)
 }
 
 export async function answerQuestion(req: express.Request, res: express.Response) {
 	let index: number = req.params.questionIndex
-	// req.session!.quiz.answers[index] = req.body['choice'];
+	if (index > req.session!.quiz.questionCount - 1) {
+		res.redirect(`/skills/summary`)
+	}
+
+	req.session!.quiz.answers[index] = req.body.answers;
 	index++
-	res.redirect(`skills/questions/${index}`)
+	res.redirect(`/skills/questions/${index}`)
 }
 
 export async function quizSummary(req: express.Request, res: express.Response) {
+	const quiz = req.session!.quiz
+	let correct: number = 0
+	quiz.questions.forEach((question: Question, index: number) => {
+		const type: string = getType(question.type)
+		question.choices.forEach((choice: Choice) => {
+			choice.type = type
+			choice.checked = quiz.answers[index] && quiz.answers[index].includes(choice.value)
+		})
+		question.correct = getDecision(question, quiz.answers[index])
+		if (question.correct) {
+			correct++
+		}
+	})
+
 	res.send(
-		template.render('skills/quiz-summary', req, res, {results: []})
+		template.render('skills/quiz-summary', req, res, {correct, quiz})
 	)
+}
+
+function getType(type: string) {
+	return type === 'MULTIPLE' ? 'checkbox' : 'radio';
+}
+
+function getDecision(question: Question, answers: any) {
+	if (!answers) {
+		return false
+	}
+	if (question.type === 'SINGLE') {
+		return answers === question.answers[0].value
+	}
+	if (question.answers.length !== answers.length) {
+		return false
+	}
+	question.answers.forEach((choice: Choice) => {
+		if (!answers.includes(choice.value)) {
+			console.log(choice.value, answers, "returning false")
+			return false
+		}
+	})
+	return true
 }
