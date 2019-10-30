d8c1da7276fea7cf27fd5e2cab9b55c6b97359a4
matthewmu <matthewmu@kainos.com>
2019-10-30 17:51:10 +0000

    fix: add compilation issue

--- src/ui/controllers/user.ts
+++ src/ui/controllers/user.ts
@@ -191,16 +191,6 @@ export function parseRoles(traversonResult: any): [Level[], string] | void {
 		return
 	}
 }
-function getNodeByName(name: string) {
-	const keys = Object.keys(nodes)
-	keys.forEach((element, index) => {
-		if (element === name) {
-			return Object.values(nodes)[index]
-		}
-	})
-
-	return ''
-}
 
 export async function newRenderAreasOfWorkPage(
 	req: express.Request,
