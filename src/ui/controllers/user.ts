e172613a2f875e223672233f982e25e2b56b35f8
Rewaz <rewaz96@hotmail.com>
2019-12-05 15:56:10 +0000

    deleted unused function

--- src/ui/controllers/user.ts
+++ src/ui/controllers/user.ts
@@ -39,17 +39,17 @@ export enum nodes {
 	'forceOrgChange' = 'forceOrgChange',
 }
 
-function getNodeByName(name: string) {
-	const keys = Object.keys(nodes)
-	let node = ''
-	keys.forEach((element, index) => {
-		if (element === name) {
-			node = Object.values(nodes)[index]
-		}
-	})
-
-	return node
-}
+// function getNodeByName(name: string) {
+// // 	const keys = Object.keys(nodes)
+// // 	let node = ''
+// // 	keys.forEach((element, index) => {
+// // 		if (element === name) {
+// // 			node = Object.values(nodes)[index]
+// // 		}
+// // 	})
+// //
+// // 	return node
+// // }
 
 const logger = log4js.getLogger('controllers/user')
 
