4d5cbe123ac6b7d89cf54518ce0182457c4530dc
Matt Doherty <matthew.doherty@kainos.com>
2020-03-18 15:08:48 +0000

    [lc-329] - Add Next to function handler definitions for profile operations to enable default error handling of errors in the handlers. When an error occurs on one of the profile update handlers, the error is passed in to the Express next function, this allows the existing default error handling to be used to handle the errors.

--- src/lib/registry.ts
+++ src/lib/registry.ts
@@ -150,7 +150,7 @@ export async function patch(node: string, data: any, token: string) {
 			})
 			.patch(data, (error, document) => {
 				if (error) {
-					reject(false)
+					resolve(false)
 				} else {
 					resolve(true)
 				}
