385cbe51409d62d0226f8b1e3cc0721f265b76ae
Rewaz <rewaz96@hotmail.com>
2020-05-05 22:39:31 +0100

    Changing Day to be 8 hours instead of 7
    I've changed a day to be 8 hours as requested by the client and deleted a function which was not being used. I also changed the test cases so that it calculated a day being 8 hours

--- src/lib/datetime.ts
+++ src/lib/datetime.ts
@@ -36,40 +36,13 @@ function ensureFullICU() {
 	}
 }
 
-export function formatCourseDurationForCourseInlcFaceToFace(d: number) {
-	if (!d) {
-		return '-'
-	}
-	let out = ''
-
-	const [hours, hourSeconds] = divmod(d, 3600)
-	if (hours) {
-		if (out) {
-			out += ' '
-		}
-		out += `${hours} hour${hours > 1 ? 's' : ''}`
-	}
-
-	const [minutes] = divmod(hourSeconds, 60)
-	if (minutes) {
-		if (out) {
-			out += ' '
-		}
-		out += `${minutes} minute${minutes > 1 ? 's' : ''}`
-	}
-	if (minutes === 0 && hours === 0 ) {
-		out += `1 minute`
-	}
-	return out
-}
-
 export function formatCourseDuration(d: number) {
 	if (!d) {
 		return '-'
 	}
 	let out = ''
 	// treat a day as 8 hours
-	const [days, daySeconds] = divmod(d, 25200)
+	const [days, daySeconds] = divmod(d, 28800)
 	if (days) {
 		out += `${days} day${days > 1 ? 's' : ''}`
 	}
