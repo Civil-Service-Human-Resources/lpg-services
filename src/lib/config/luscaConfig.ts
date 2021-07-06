a84b1766174c42a1164a2caa3aa8f77889ebd183
Jamie Lovell <jamie.lovell@cabinetoffice.gov.uk>
2021-07-06 11:27:27 +0100

    corrected youtube URLs

--- src/lib/config/luscaConfig.ts
+++ src/lib/config/luscaConfig.ts
@@ -15,13 +15,15 @@ export function setCspPolicy(staticAssetDomain: string) {
 		staticCdn = ''
 	}
 
+	const youtubeUrls = 'https://youtube.com https://www.youtube.com'
+
 	const policy =  {
-		'child-src': 'https://youtube.com https://www.youtube.com',
+		'child-src': youtubeUrls,
 		'default-src': `'self' ${contentCdn} ${staticCdn}`.trim(),
 		'font-src': `'self' data: ${staticCdn}`.trim(),
-		'frame-src': 'https://youtube.com https://www.youtube.com',
+		'frame-src': youtubeUrls,
 		'img-src': `'self' data: https://www.google-analytics.com ${contentCdn} ${staticCdn}`.trim(),
-		'media-src': 'https://youtube.com',
+		'media-src': youtubeUrls,
 		'script-src':
 		// tslint:disable-next-line:max-line-length
 		`'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://www.youtube.com https://s.ytimg.com ${staticCdn}`.trim(),
