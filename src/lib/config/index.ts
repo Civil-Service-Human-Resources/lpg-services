c07028c78c78a113b039ee1d71daecd92a0c0e7b
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-03-31 16:08:09 +0100

    LC-1669 feedback link updated to use Qualtrics

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -140,3 +140,5 @@ export const STATIC_ASSET_ROOT = env.STATIC_ASSET_ROOT
 export const STATIC_ASSET_TTL = env.STATIC_ASSET_TTL
 
 export const TOKEN_EXPIRY_BUFFER = Number(env.TOKEN_EXPIRY_BUFFER) || 30
+
+export const FEEDBACK_URL = env.FEEDBACK_URL || 'https://civilserviceinsight.qualtrics.com/jfe/form/SV_cHYL88D3Goop5CS'
