856f4b2d4f6bc5705d135201e387e4706b19c857
SavvasCO <savvas.stephanides@cabinetoffice.gov.uk>
2023-04-20 12:03:12 +0100

    Fix course completion bug

--- src/ui/controllers/home.ts
+++ src/ui/controllers/home.ts
@@ -53,6 +53,8 @@ export async function home(req: express.Request, res: express.Response, next: ex
 							i -= 1
 						} else {
 							if (previousRequiredBy) {
+								console.log({previousRequiredBy, earliestCompletionDateOfModulesForACourse, latestCompletionDateOfModulesForACourse});
+								
 								if (earliestCompletionDateOfModulesForACourse && latestCompletionDateOfModulesForACourse
 									&& previousRequiredBy < earliestCompletionDateOfModulesForACourse
 									&& previousRequiredBy < latestCompletionDateOfModulesForACourse) {
