bda9757b549f1a8940685fc755874393aeac40e8
SavvasCO <savvas.stephanides@cabinetoffice.gov.uk>
2023-06-16 14:04:27 +0100

    Video fix

--- src/lib/config/luscaConfig.ts
+++ src/lib/config/luscaConfig.ts
@@ -1,4 +1,13 @@
+import * as config from "lib/config/index"
+
 export function setCspPolicy(staticAssetDomain: string) {
+
+	let contentCdn
+	if (config.PROFILE === 'prod') {
+		contentCdn = 'https://cdn.learn.civilservice.gov.uk'
+	} else {
+		contentCdn = `https://cdn.${config.PROFILE}.learn.civilservice.gov.uk`
+	}
 
 	let staticCdn: string
 	if (staticAssetDomain) {
@@ -9,10 +18,10 @@ export function setCspPolicy(staticAssetDomain: string) {
 
 	const policy =  {
 		'child-src': 'https://youtube.com https://www.youtube.com',
-		'default-src': `'self' ${staticCdn}`.trim(),
+		'default-src': `'self' ${contentCdn} ${staticCdn}`.trim(),
 		'font-src': `'self' data: ${staticCdn}`.trim(),
 		'frame-src': 'https://youtube.com https://www.youtube.com',
-		'img-src': `'self' data: https://www.google-analytics.com ${staticCdn}`.trim(),
+		'img-src': `'self' data: https://www.google-analytics.com ${contentCdn} ${staticCdn}`.trim(),
 		'script-src':
 		// tslint:disable-next-line:max-line-length
 		`'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://www.youtube.com https://s.ytimg.com ${staticCdn}`.trim(),
