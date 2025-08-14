dc6ba3f1e42f8b2ca3e7dee010ea5de0311b6e47
Jamie <jamie.lovell@cabinetofice.gov.uk>
2025-08-14 16:10:38 +0100

    Remove old organisational unit typeahead code

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,6 @@ import * as csrsService from './lib/service/civilServantRegistry/csrsService'
 import {Grades} from './lib/service/civilServantRegistry/grade/grades'
 import {Interests} from './lib/service/civilServantRegistry/interest/interests'
 import {OrganisationalUnitCache} from './lib/service/civilServantRegistry/organisationalUnit/organisationalUnitCache'
-import {OrganisationalUnitTypeaheadCache} from './lib/service/civilServantRegistry/organisationalUnit/organisationalUnitTypeaheadCache'
 import {LearningRecordCache} from './lib/service/cslService/cache/learningRecordCache'
 import {RequiredLearningCache} from './lib/service/cslService/cache/RequiredLearningCache'
 import * as cslService from './lib/service/cslService/cslServiceClient'
@@ -106,12 +105,11 @@ app.use(
 )
 
 const orgCache = new OrganisationalUnitCache(redisClient, config.ORG_REDIS.defaultTTL)
-const orgTypeaheadCache = new OrganisationalUnitTypeaheadCache(redisClient, config.ORG_REDIS.defaultTTL)
 const csrsProfileCache = new ProfileCache(redisClient, config.PROFILE_REDIS.defaultTTL)
 const gradeCache = new AnonymousCache(redisClient, config.GRADE_REDIS.defaultTTL, 'grades', Grades)
 const areaOfWorkCache = new AnonymousCache(redisClient, config.AOW_REDIS.defaultTTL, 'areasOfWork', AreasOfWork)
 const interestCache = new AnonymousCache(redisClient, config.INTEREST_REDIS.defaultTTL, 'Interests', Interests)
-csrsService.setCaches(orgCache, orgTypeaheadCache, csrsProfileCache, gradeCache, areaOfWorkCache, interestCache)
+csrsService.setCaches(orgCache, csrsProfileCache, gradeCache, areaOfWorkCache, interestCache)
 
 const formattedOrganisationListCache = new FormattedOrganisationListCache(
 	redisClient,
