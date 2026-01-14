978f0b5b1cdd045e91f57ceb5e527cddfe3af355
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2026-01-14 16:50:47 +0000

    LC-2998-Prevent double click on module launch links

--- views/assets/js/prevent-double-click.js
+++ views/assets/js/prevent-double-click.js
@@ -7,17 +7,27 @@
 
 	GOVUK.Modules.PreventDoubleClick = function () {
 		this.start = function (element) {
-			element.on('click', function (e) {
-				if (element.data('clicked')) {
-					e.preventDefault()
-					e.stopPropagation()
-				} else {
-					element.data('clicked', true)
-					setTimeout(function () {
-						element.prop('disabled', true)
-					}, 0)
-				}
-			})
+			// Ensure we are working with the form element
+			var $form = element.is('form') ? element : element.closest('form')
+
+			if ($form.length > 0) {
+				$form.on('submit', function (e) {
+					if ($form.data('submitted')) {
+						e.preventDefault()
+						e.stopPropagation()
+					} else {
+						$form.data('submitted', true)
+
+						// If opening in a new tab, reset the lock after a few seconds
+						// so the user can try again if needed.
+						if ($form.attr('target') === '_blank') {
+							setTimeout(function () {
+								$form.data('submitted', false)
+							}, 4000)
+						}
+					}
+				})
+			}
 		}
 	}
 
