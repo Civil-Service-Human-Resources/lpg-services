b24a04d9f3ce2ba642f2e483fdf5caab4244b5a1
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2026-01-15 14:24:32 +0000

    LC-2998-Prevent double click on module launch links

--- views/assets/js/prevent-double-click.js
+++ views/assets/js/prevent-double-click.js
@@ -1,35 +1,30 @@
-;(function (global) {
+;(function (Modules) {
 	'use strict'
 
-	var $ = global.jQuery
-	var GOVUK = global.GOVUK || {}
-	GOVUK.Modules = GOVUK.Modules || {}
-
-	GOVUK.Modules.PreventDoubleClick = function () {
-		this.start = function (element) {
-			// Ensure we are working with the form element
-			var $form = element.is('form') ? element : element.closest('form')
-
-			if ($form.length > 0) {
-				$form.on('submit', function (e) {
-					if ($form.data('submitted')) {
-						e.preventDefault()
-						e.stopPropagation()
-					} else {
-						$form.data('submitted', true)
-
-						// If opening in a new tab, reset the lock after a few seconds
-						// so the user can try again if needed.
-						if ($form.attr('target') === '_blank') {
-							setTimeout(function () {
-								$form.data('submitted', false)
-							}, 4000)
-						}
-					}
-				})
-			}
-		}
+	function DiscitePreventDoubleClick(module) {
+		this.module = module
 	}
 
-	global.GOVUK = GOVUK
-})(window)
+	DiscitePreventDoubleClick.prototype.init = function () {
+		var button = this.module
+
+		console.log(
+			'DiscitePreventDoubleClick initialised for module:',
+			button.dataset.url,
+			'newTab:',
+			button.dataset.newTab
+		)
+
+		button.addEventListener('click', function () {
+			if (button.dataset.clicked === 'true') {
+				return
+			}
+
+			button.dataset.clicked = 'true'
+			button.setAttribute('aria-disabled', 'true')
+			button.classList.add('is-disabled')
+		})
+	}
+
+	Modules.DiscitePreventDoubleClick = DiscitePreventDoubleClick
+})(window.GOVUK.Modules)
