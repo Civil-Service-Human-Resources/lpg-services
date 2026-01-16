b5188d19ee3b39a76926d84a6aca96c8c3e53ce3
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2026-01-16 15:57:34 +0000

    LC-2998 -Prevent double click on module launch links

--- views/assets/js/prevent-double-click.js
+++ views/assets/js/prevent-double-click.js
@@ -13,9 +13,17 @@
 				return
 			}
 
+			// Disable the button immediately
 			button.dataset.clicked = 'true'
 			button.setAttribute('aria-disabled', 'true')
 			button.classList.add('is-disabled')
+
+			// Re-enable the button after 2 seconds (2000ms)
+			setTimeout(function () {
+				button.dataset.clicked = 'false'
+				button.removeAttribute('aria-disabled')
+				button.classList.remove('is-disabled')
+			}, 2000)
 		})
 	}
 
