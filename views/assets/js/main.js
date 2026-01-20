0f7fbd3c96354f475b70b1b30a5242aee87635bf
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2026-01-20 15:17:44 +0000

    LC-2998-Prevent double click on module launch links

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -1,7 +1,3 @@
-let $ = require('jquery')
-window.$ = $
-window.jQuery = $
-
 require('./govuk_frontend_toolkit/govuk/show-hide-content')
 require('./govuk_frontend_toolkit/govuk/modules')
 require('./govuk_frontend_toolkit/govuk/govuk-template')
@@ -10,6 +6,21 @@ require('./cookies')
 require('./typeahead')
 require('./prevent-double-click')
 
-$(document).ready(function () {
-	GOVUK.modules.start()
+document.addEventListener('DOMContentLoaded', function () {
+	let modules = document.querySelectorAll('[data-module]')
+
+	modules.forEach(function (element) {
+		let moduleName = element
+			.getAttribute('data-module')
+			.split('-')
+			.map(function (word) {
+				return word.charAt(0).toUpperCase() + word.slice(1)
+			})
+			.join('')
+
+		if (window.GOVUK && window.GOVUK.Modules && typeof window.GOVUK.Modules[moduleName] === 'function') {
+			let module = new window.GOVUK.Modules[moduleName]()
+			module.start(element)
+		}
+	})
 })
--- views/assets/js/prevent-double-click.js
+++ views/assets/js/prevent-double-click.js
@@ -1,30 +1,44 @@
 ;(function (Modules) {
 	'use strict'
 
-	function DiscitePreventDoubleClick() {}
+	function DiscitePreventDoubleClick() {
+		this.debounceFormSubmitTimer = null
+	}
 
-	DiscitePreventDoubleClick.prototype.start = function ($element) {
-		let button = $element[0]
+	DiscitePreventDoubleClick.prototype.start = function ($module) {
+		this.$module = $module instanceof Node ? $module : $module[0]
 
-		button.addEventListener('click', function (e) {
-			if (button.dataset.clicked === 'true') {
-				e.preventDefault()
-				e.stopPropagation()
-				return
-			}
+		if (!this.$module) {
+			return
+		}
 
-			// Disable the button immediately
-			button.dataset.clicked = 'true'
-			button.setAttribute('aria-disabled', 'true')
-			button.classList.add('is-disabled')
+		this.init()
+	}
 
-			// Re-enable the button after 2 seconds (2000ms)
-			setTimeout(function () {
-				button.dataset.clicked = 'false'
-				button.removeAttribute('aria-disabled')
-				button.classList.remove('is-disabled')
-			}, 2000)
-		})
+	DiscitePreventDoubleClick.prototype.init = function () {
+		var timeoutAttr = this.$module.getAttribute('data-timeout')
+		this.preventionTimeout = timeoutAttr ? parseInt(timeoutAttr, 10) : 2000
+		this.$module.addEventListener('click', this.handleClick.bind(this))
+	}
+
+	DiscitePreventDoubleClick.prototype.handleClick = function (event) {
+		if (this.$module.getAttribute('data-clicked') === 'true') {
+			event.preventDefault()
+			event.stopPropagation()
+			return
+		}
+
+		this.$module.setAttribute('data-clicked', 'true')
+		this.$module.setAttribute('aria-disabled', 'true')
+		this.$module.classList.add('is-disabled')
+
+		this.debounceFormSubmitTimer = setTimeout(this.reset.bind(this), this.preventionTimeout)
+	}
+
+	DiscitePreventDoubleClick.prototype.reset = function () {
+		this.$module.setAttribute('data-clicked', 'false')
+		this.$module.removeAttribute('aria-disabled')
+		this.$module.classList.remove('is-disabled')
 	}
 
 	Modules.DiscitePreventDoubleClick = DiscitePreventDoubleClick
