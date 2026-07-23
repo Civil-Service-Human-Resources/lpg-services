a4b6f44c3bcebf424f769c713ce56e6b8f4e874d
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-07-23 11:42:35 +0100

    Advanced column layout css

--- src/ui/controllers/nsg/controller.ts
+++ src/ui/controllers/nsg/controller.ts
@@ -5,17 +5,18 @@ import {getCategoryHomepage, getCategoryPage} from '../../../lib/service/cslServ
 import {Category} from '../../../lib/service/cslService/models/learning/categories/category'
 import * as asyncHandler from 'express-async-handler'
 
+
 export const router = Router()
 
 router.all('*', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-	if (!(req.user as User).hasRole('LEARNING_TAG_MANAGER')) {
-		return res.redirect('/')
-	}
-	next()
+		if (!(req.user as User).hasRole('LEARNING_TAG_MANAGER')) {
+			return res.redirect('/')
+		}
+		next()
 })
 
 router.get('/nsg-homepage', asyncHandler(index))
-router.get('/nsg-homepage/categories/:url', asyncHandler(categoryPage))
+router.get('/nsg-homepage/:url', asyncHandler(categoryPage))
 
 export async function index(req: express.Request, res: express.Response) {
 	const homepage = await getCategoryHomepage(req.user)
